a857af9e042cce95bb6b7db81b1b8786d42ca496
Gitnube <dmitriyvbyf.comp@mail.ru>
2019-05-16 23:08:28 +0700

    Добавлена документация для setSoundFileName() и getSoundFileName

--- alarmclock.h
+++ alarmclock.h
@@ -7,7 +7,15 @@ class AlarmClock
 {
 public:
     AlarmClock();
+    /*!
+    Устанавливает название файла мелодии будильника
+    \param fileName название файла мелодии
+    */
     void setSoundFileName(QString fileName);
+    /*!
+    Возвращает название файла мелодии будильника
+    \return название файла мелодии
+    */
     QString getSoundFileName();
 private:
     bool fileNameType;
