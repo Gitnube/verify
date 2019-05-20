c6c4ee690c10f847b45243f1e32869e317fa145d
Gitnube <dmitriyvbyf.comp@mail.ru>
2019-05-20 22:15:58 +0700

    Рефакторинг launch(), мелодия не воспроизводится до назначенного через 1 сек., 1.5 сек., 1.6 сек. времени

--- alarmclock.cpp
+++ alarmclock.cpp
@@ -33,11 +33,7 @@ QMediaPlayer::State AlarmClock::getPlayingState() const
 
 void AlarmClock::launch()
 {
-    int interval;
     QTimer timer;
     player.setMedia(QUrl::fromLocalFile(QDir::currentPath() + "/alarm.mp3"));
-    if(QTime::currentTime().msecsTo(alarmTime) > 1000)
-        interval = 1500;
-    else interval = 1000;
-    timer.singleShot(interval,&player,SLOT(play()));
+    timer.singleShot(QTime::currentTime().msecsTo(alarmTime),&player,SLOT(play()));
 }
